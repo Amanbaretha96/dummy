b9a697fb638467de81270d66801b8537b6a9b901
mnjjhfvk <amanbaretha96@gmail.com>
2024-03-07 08:40:41 +0530

    second commit

--- src/component/secondcomponent.jsx
+++ src/component/secondcomponent.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Secondpage from "../pages/secondpage";
 
 const Secondcomponent = () => {
+    console.log("testing git branch")
     const[aman,setAman] = useState(0)
 
     return(
